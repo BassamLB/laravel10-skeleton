3c624b15a6116054681c43f39a8c0cbe254e8c3a
BassamLB <Baawar@hotmail.com>
2023-02-16 22:14:08 +0200

    prevent navigation Back after logout

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,26 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, router } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+// added By Bassam To prevent navigation back after logout
+
+router.on('success', (event) => {
+    let isAuthenticated = event.detail.page.props.auth.user !== null;
+    window.localStorage.setItem('isAuthenticated', isAuthenticated);
+});
+
+window.addEventListener('popstate', (event) => {
+    if(window.localStorage.getItem('isAuthenticated') === 'false') {
+        event.stopImmediatePropagation();
+        router.get('/login');
+    }
+});
+// end of after logout
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
